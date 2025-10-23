1b5a7f36c6ce8f4a3ca8b254747a0c7ddf11d605
ujwaldas <ujwaldas007@gmail.com>
2025-10-23 08:41:09 +0530

    Top K freq element from after studying heap q solution

--- arrays/top_k_elment.py
+++ arrays/top_k_elment.py
@@ -1,3 +1,17 @@
+# class Solution(object):
+#     def topKFrequent(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: List[int]
+#         """
+#         freq = {}
+#         for num in nums:
+#             freq[num] = freq.get(num,0) + 1 
+#         sortedFreq = sorted(freq.items(),key=lambda x: x[1], reverse=True )
+#         return [num for num,count in sortedFreq[:k]]
+
+
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -8,8 +22,8 @@ class Solution(object):
         freq = {}
         for num in nums:
             freq[num] = freq.get(num,0) + 1 
-        sortedFreq = sorted(freq.items(),key=lambda x: x[1], reverse=True )
-        return [num for num,count in sortedFreq[:k]]
-
+        
+        return heapq.nlargest(k,freq.keys(),freq.get)
+      
 if __name__ == "__main__":
     print(Solution().topKFrequent([1,1,1,2,2,3], 2))
