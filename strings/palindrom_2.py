979a60daf439b2ed904f88ab313a681b2fd843f7
ujwaldas <ujwaldas007@gmail.com>
2025-10-24 08:48:54 +0530

    palindrom 2 not optimised solution

---
+++ strings/palindrom_2.py
@@ -0,0 +1,65 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        palindrome = True
+        skip = False
+        right = len(s) - 1
+        if length == 0:
+            return palindrome
+        i = 0
+
+        while i <= length:
+            if s[i] == s[length]:
+                palindrome = True
+            elif s[i] == s[length-1] and skip == False:
+                palindrome = True
+                skip = True
+                length -= 1
+            elif s[i+1] == s[length] and skip == False:
+                palindrome = True
+                skip = True
+                i += 1
+            else:
+                palindrome = False
+                break
+            i += 1
+            length -= 1
+        if palindrome == True:
+            return palindrome
+        length = len(s) - 1
+        i = 0
+        palindrome = True
+        skip = False
+        while i <= length:
+            if s[i] == s[length]:
+                palindrome = True
+            elif s[i+1] == s[length] and skip == False:
+                palindrome = True
+                skip = True
+                i += 1
+            elif s[i] == s[length-1] and skip == False:
+                palindrome = True
+                skip = True
+                length -= 1
+            else:
+                palindrome = False
+                break
+            i += 1
+            length -= 1
+
+        print(palindrome)
+        return palindrome
+        # for i in range(len(s)):
+        #     elementRemoved = s[:i] + s[i+1:]
+        #     if s[::-1] == s:
+        #         print(s[::-1] == s)
+        #         return True
+        #     if elementRemoved == elementRemoved[::-1]:
+        #         return True
+        # return False
+            
+
+        
